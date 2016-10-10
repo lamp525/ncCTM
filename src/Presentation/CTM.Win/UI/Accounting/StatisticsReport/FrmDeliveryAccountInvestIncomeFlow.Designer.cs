2f48041ff7b6398ced9ab446f4b3b85132e68790
Ethan Colin <epoch0ash@gmail.com>
2016-10-10 13:11:27 +0800

    fix typo

--- src/Presentation/CTM.Win/UI/Accounting/StatisticsReport/FrmDeliveryAccountInvestIncomeFlow.Designer.cs
+++ src/Presentation/CTM.Win/UI/Accounting/StatisticsReport/FrmDeliveryAccountInvestIncomeFlow.Designer.cs
@@ -563,7 +563,7 @@
             this.ClientSize = new System.Drawing.Size(1524, 851);
             this.Controls.Add(this.layoutControl1);
             this.Name = "FrmDeliveryAccountInvestIncomeFlow";
-            this.Text = "FrmAccountInvestIncomeFlow";
+            this.Text = "FrmDeliveryAccountInvestIncomeFlow";
             this.Load += new System.EventHandler(this.FrmAccountInvestIncomeFlow_Load);
             ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
             this.layoutControl1.ResumeLayout(false);
