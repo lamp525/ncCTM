713ca575d15e879c59e2f15e9a4c203cef374201
Ethan Colin <epoch0ash@gmail.com>
2016-10-12 10:29:49 +0800

    Initial commit

--- src/Libraries/CTM.Core/Domain/Account/AccountFundTransfer.cs
+++ src/Libraries/CTM.Core/Domain/Account/AccountFundTransfer.cs
@@ -2,25 +2,23 @@
 
 namespace CTM.Core.Domain.Account
 {
-    public class AccountTradeFund : BaseEntity
+    public class AccountFundTransfer : BaseEntity
     {
+        public int AccountId { get; set; }
+
         public string AccountCode { get; set; }
 
         public DateTime Time { get; set; }
 
         public string Operator { get; set; }
 
-        public int TradeType { get; set; }
+        public int TransferType { get; set; }
 
         public bool FlowType { get; set; }
 
-        public string ObjectAccountCode { get; set; }
+        public int TargetAccountId { get; set; }
 
-        public int Volume { get; set; }
-
-        public decimal TradePrice { get; set; }
-
-        public decimal CostPrice { get; set; }
+        public string TargetAccountCode { get; set; }
 
         public decimal Amount { get; set; }
 
---
+++ src/Presentation/CTM.Win/UI/Accounting/AccountManage/FrmAccountFundTransfer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTM.Win.UI.Accounting.AccountManage
+{
+    public partial class FrmAccountFundTransfer : BaseForm
+    {
+        public FrmAccountFundTransfer()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmAccountFund_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
