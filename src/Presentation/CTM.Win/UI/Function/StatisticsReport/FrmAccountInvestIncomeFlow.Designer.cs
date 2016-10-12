ae967403fe46f2ceb0521744c17bac53d3738c3b
Ethan Colin <epoch0ash@gmail.com>
2016-10-12 11:19:41 +0800

    rename Account Combobox Text

--- src/Presentation/CTM.Win/UI/Function/StatisticsReport/FrmAccountInvestIncomeFlow.Designer.cs
+++ src/Presentation/CTM.Win/UI/Function/StatisticsReport/FrmAccountInvestIncomeFlow.Designer.cs
@@ -116,7 +116,7 @@
             // 
             // btnSaveLayout
             // 
-            this.btnSaveLayout.Location = new System.Drawing.Point(1418, 112);
+            this.btnSaveLayout.Location = new System.Drawing.Point(1418, 116);
             this.btnSaveLayout.Name = "btnSaveLayout";
             this.btnSaveLayout.Size = new System.Drawing.Size(82, 22);
             this.btnSaveLayout.StyleController = this.layoutControl1;
@@ -126,10 +126,10 @@
             // 
             // gridControl1
             // 
-            this.gridControl1.Location = new System.Drawing.Point(24, 138);
+            this.gridControl1.Location = new System.Drawing.Point(24, 142);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(1476, 689);
+            this.gridControl1.Size = new System.Drawing.Size(1476, 685);
             this.gridControl1.TabIndex = 6;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -325,7 +325,7 @@
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(1127, 43);
+            this.btnSearch.Location = new System.Drawing.Point(1127, 45);
             this.btnSearch.Name = "btnSearch";
             this.btnSearch.Size = new System.Drawing.Size(77, 22);
             this.btnSearch.StyleController = this.layoutControl1;
@@ -336,7 +336,7 @@
             // deEndTradeDate
             // 
             this.deEndTradeDate.EditValue = null;
-            this.deEndTradeDate.Location = new System.Drawing.Point(310, 43);
+            this.deEndTradeDate.Location = new System.Drawing.Point(310, 45);
             this.deEndTradeDate.Name = "deEndTradeDate";
             this.deEndTradeDate.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -348,7 +348,7 @@
             // 
             // luAccount
             // 
-            this.luAccount.Location = new System.Drawing.Point(756, 43);
+            this.luAccount.Location = new System.Drawing.Point(756, 45);
             this.luAccount.Name = "luAccount";
             this.luAccount.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -364,7 +364,7 @@
             // 
             // cbAccountType
             // 
-            this.cbAccountType.Location = new System.Drawing.Point(533, 43);
+            this.cbAccountType.Location = new System.Drawing.Point(533, 45);
             this.cbAccountType.Name = "cbAccountType";
             this.cbAccountType.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -376,7 +376,7 @@
             // deStartTradeDate
             // 
             this.deStartTradeDate.EditValue = null;
-            this.deStartTradeDate.Location = new System.Drawing.Point(87, 43);
+            this.deStartTradeDate.Location = new System.Drawing.Point(87, 45);
             this.deStartTradeDate.Name = "deStartTradeDate";
             this.deStartTradeDate.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
             new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
@@ -416,7 +416,7 @@
             this.emptySpaceItem2});
             this.layoutControlGroup2.Location = new System.Drawing.Point(0, 0);
             this.layoutControlGroup2.Name = "layoutControlGroup2";
-            this.layoutControlGroup2.Size = new System.Drawing.Size(1504, 69);
+            this.layoutControlGroup2.Size = new System.Drawing.Size(1504, 71);
             this.layoutControlGroup2.Text = "查询条件";
             // 
             // layoutControlItem2
@@ -426,7 +426,7 @@
             this.layoutControlItem2.Location = new System.Drawing.Point(669, 0);
             this.layoutControlItem2.Name = "layoutControlItem2";
             this.layoutControlItem2.Size = new System.Drawing.Size(422, 26);
-            this.layoutControlItem2.Text = "交易账户";
+            this.layoutControlItem2.Text = "账户信息";
             this.layoutControlItem2.TextSize = new System.Drawing.Size(60, 14);
             // 
             // layoutControlItem1
@@ -516,9 +516,9 @@
             this.layoutControlItem5,
             this.emptySpaceItem6,
             this.layoutControlItem6});
-            this.layoutControlGroup3.Location = new System.Drawing.Point(0, 69);
+            this.layoutControlGroup3.Location = new System.Drawing.Point(0, 71);
             this.layoutControlGroup3.Name = "layoutControlGroup3";
-            this.layoutControlGroup3.Size = new System.Drawing.Size(1504, 762);
+            this.layoutControlGroup3.Size = new System.Drawing.Size(1504, 760);
             this.layoutControlGroup3.Text = "投资收益明细（金额单位：元）";
             // 
             // layoutControlItem5
@@ -526,7 +526,7 @@
             this.layoutControlItem5.Control = this.gridControl1;
             this.layoutControlItem5.Location = new System.Drawing.Point(0, 26);
             this.layoutControlItem5.Name = "layoutControlItem5";
-            this.layoutControlItem5.Size = new System.Drawing.Size(1480, 693);
+            this.layoutControlItem5.Size = new System.Drawing.Size(1480, 689);
             this.layoutControlItem5.TextLocation = DevExpress.Utils.Locations.Top;
             this.layoutControlItem5.TextSize = new System.Drawing.Size(0, 0);
             this.layoutControlItem5.TextVisible = false;
