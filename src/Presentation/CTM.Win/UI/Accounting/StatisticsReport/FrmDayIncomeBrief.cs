e71113214eb6a2e519619395b8ad52366187fde2
Ethan Colin <epoch0ash@gmail.com>
2016-10-13 08:59:27 +0800

    Initial commit

---
+++ src/Presentation/CTM.Win/UI/Accounting/StatisticsReport/FrmDayIncomeBrief.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTM.Win.UI.Accounting.StatisticsReport
+{
+    public partial class FrmDayIncomeBrief : BaseForm
+    {
+        public FrmDayIncomeBrief()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmDayIncomeBrief_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
