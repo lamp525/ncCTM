32858438a655ef1dbf080f2a70a9e5db2edcd509
Ethan Colin <epoch0ash@gmail.com>
2016-10-20 16:49:42 +0800

    add field[UpdateTime]

--- src/Libraries/CTM.Core/Domain/InvestmentDecision/InvestmentDecisionForm.cs
+++ src/Libraries/CTM.Core/Domain/InvestmentDecision/InvestmentDecisionForm.cs
@@ -34,6 +34,8 @@ namespace CTM.Core.Domain.InvestmentDecision
 
         public DateTime CreateTime { get; set; }
 
+        public DateTime UpdateTime { get; set; }
+
         public string Reason { get; set; }
     }
 }
