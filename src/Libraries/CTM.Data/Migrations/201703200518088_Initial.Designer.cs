bd76e8ea0e7dd47b0f378c8b902870a7bc458baf
Ethan Colin <epoch0ash@gmail.com>
2017-03-20 13:37:29 +0800

    DB Initial

--- src/Libraries/CTM.Data/Migrations/201703200518088_Initial.Designer.cs
+++ src/Libraries/CTM.Data/Migrations/201703200518088_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace CTM.Data.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class _004 : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(_004));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201610190812116_004"; }
+            get { return "201703200518088_Initial"; }
         }
         
         string IMigrationMetadata.Source
