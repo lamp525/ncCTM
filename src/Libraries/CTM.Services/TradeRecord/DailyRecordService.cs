aeda5b689df824c1ebf89a126a94f15257c1ca96
Ethan Colin <epoch0ash@gmail.com>
2017-07-12 15:12:35 +0800

    NO DESCRIPTION

--- src/Libraries/CTM.Services/TradeRecord/DailyRecordService.cs
+++ src/Libraries/CTM.Services/TradeRecord/DailyRecordService.cs
@@ -669,17 +669,17 @@ namespace CTM.Services.TradeRecord
             columnList.Add(nameof(record.TradeTime), "委托时间");
             columnList.Add(nameof(record.StockCode), "证券代码");
             columnList.Add(nameof(record.StockName), "证券名称");
-            columnList.Add(nameof(record.DealFlag), "操作");
+            columnList.Add(nameof(record.DealFlag), "买卖标志");
             columnList.Add(nameof(record.EntrustVolume), "委托数量");
             columnList.Add(nameof(record.EntrustPrice), "委托价格");
             columnList.Add(nameof(record.EntrustAmount), null);
-            columnList.Add(nameof(record.DealPrice), "成交均价");
+            columnList.Add(nameof(record.DealPrice), "委托价格");
             columnList.Add(nameof(record.DealVolume), "成交数量");
-            columnList.Add(nameof(record.DealAmount), "成交金额");
-            columnList.Add(nameof(record.StockHolderCode), "股东帐户");
-            columnList.Add(nameof(record.DealNo), "委托序号");
-            columnList.Add(nameof(record.ContractNo), "委托序号");
-            columnList.Add(nameof(record.Remarks), "操作");
+            columnList.Add(nameof(record.DealAmount), null);
+            columnList.Add(nameof(record.StockHolderCode), "股东代码");
+            columnList.Add(nameof(record.DealNo), "委托编号");
+            columnList.Add(nameof(record.ContractNo), "委托编号");
+            columnList.Add(nameof(record.Remarks), "买卖标志");
             columnList.Add(nameof(record.TradeType), "交易类别");
 
             List<string> templateColumnNames = columnList.Values.Where(x => !string.IsNullOrEmpty(x)).ToList();
