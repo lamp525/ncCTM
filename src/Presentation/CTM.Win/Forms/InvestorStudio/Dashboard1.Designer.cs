446c4cacce57ea4e0cb9588807841d8fb998ba81
Ethan Colin <epoch0ash@gmail.com>
2018-01-02 13:34:05 +0800

    NO DESCRIPTION

--- src/Presentation/CTM.Win/Forms/InvestorStudio/Dashboard1.Designer.cs
+++ src/Presentation/CTM.Win/Forms/InvestorStudio/Dashboard1.Designer.cs
@@ -30,8 +30,11 @@
         {
             DevExpress.DashboardCommon.DashboardLayoutGroup dashboardLayoutGroup1 = new DevExpress.DashboardCommon.DashboardLayoutGroup();
             DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem1 = new DevExpress.DashboardCommon.DashboardLayoutItem();
+            DevExpress.DashboardCommon.DashboardLayoutItem dashboardLayoutItem2 = new DevExpress.DashboardCommon.DashboardLayoutItem();
             this.cardDashboardItem1 = new DevExpress.DashboardCommon.CardDashboardItem();
+            this.cardDashboardItem2 = new DevExpress.DashboardCommon.CardDashboardItem();
             ((System.ComponentModel.ISupportInitialize)(this.cardDashboardItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardDashboardItem2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
             // cardDashboardItem1
@@ -42,19 +45,32 @@
             this.cardDashboardItem1.Name = "Cards 1";
             this.cardDashboardItem1.ShowCaption = true;
             // 
+            // cardDashboardItem2
+            // 
+            this.cardDashboardItem2.ComponentName = "cardDashboardItem2";
+            this.cardDashboardItem2.DataItemRepository.Clear();
+            this.cardDashboardItem2.InteractivityOptions.IgnoreMasterFilters = false;
+            this.cardDashboardItem2.Name = "Cards 2";
+            this.cardDashboardItem2.ShowCaption = true;
+            // 
             // Dashboard1
             // 
             this.Items.AddRange(new DevExpress.DashboardCommon.DashboardItem[] {
-            this.cardDashboardItem1});
-            dashboardLayoutItem1.DashboardItem = this.cardDashboardItem1;
-            dashboardLayoutItem1.Weight = 100D;
+            this.cardDashboardItem1,
+            this.cardDashboardItem2});
+            dashboardLayoutItem1.DashboardItem = this.cardDashboardItem2;
+            dashboardLayoutItem1.Weight = 49.939098660170522D;
+            dashboardLayoutItem2.DashboardItem = this.cardDashboardItem1;
+            dashboardLayoutItem2.Weight = 50.060901339829478D;
             dashboardLayoutGroup1.ChildNodes.AddRange(new DevExpress.DashboardCommon.DashboardLayoutNode[] {
-            dashboardLayoutItem1});
+            dashboardLayoutItem1,
+            dashboardLayoutItem2});
             dashboardLayoutGroup1.DashboardItem = null;
             dashboardLayoutGroup1.Orientation = DevExpress.DashboardCommon.DashboardLayoutGroupOrientation.Vertical;
             this.LayoutRoot = dashboardLayoutGroup1;
             this.Title.Text = "Dashboard";
             ((System.ComponentModel.ISupportInitialize)(this.cardDashboardItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cardDashboardItem2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
         }
@@ -62,5 +78,6 @@
         #endregion
 
         private DevExpress.DashboardCommon.CardDashboardItem cardDashboardItem1;
+        private DevExpress.DashboardCommon.CardDashboardItem cardDashboardItem2;
     }
 }
