a4e6754e377e711e4f26dacba0d97aa607dd66b6
Ethan Colin <epoch0ash@gmail.com>
2018-07-13 10:31:16 +0800

    Add FrmAccountRC

---
+++ src/Presentation/CTM.Win/Forms/DailyTrading/RiskControl/FrmAccountRC.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CTM.Win.Forms.DailyTrading.RiskControl
+{
+    public partial class FrmAccountRC : Form
+    {
+        public FrmAccountRC()
+        {
+            InitializeComponent();
+        }
+    }
+}
